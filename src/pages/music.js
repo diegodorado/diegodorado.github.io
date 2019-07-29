da7b7d6536300a80fed67795c671c05a9b56ac43
Diego Dorado <diegodorado@gmail.com>
2019-07-29 02:30:39 -0300

    updates icons

--- src/pages/music.js
+++ src/pages/music.js
@@ -3,18 +3,104 @@ import { graphql } from "gatsby"
 import Layout from "../layouts/main"
 import SEO from "../components/seo"
 import { Trans } from 'react-i18next'
+//import prettyTime from '../components/prettyTime'
 
-import { FaVolumeOff} from 'react-icons/fa/index.esm.js'
+import { FaStepBackward,
+         FaStepForward,
+         FaPause,
+         FaPlay,
+         FaVolumeOff as FaVolumeOn,
+         FaVolumeMute as FaVolumeOff } from 'react-icons/fa/index.esm.js'
 
 
+//import SoundCloudAudio from 'soundcloud-audio'
+
+const prettyTime = (time) => {return "00:00"}
+
+const clientId = '802921cdc630a9a0d66f25c665703b8c'
 
 const MusicIndex = ({ data, location }) => {
   const tracks = data.allSoundcloudtrack.edges
+  const [player, setPlayer] = useState(null)
   const [index, setIndex] = useState(0)
   const [track, setTrack] = useState(tracks[0].node)
+  const [volume, setVolume] = useState(100)
+  const [muted, setMuted] = useState(false)
+  const [currentTime, setCurrentTime] = useState(0)
+
+  const toggle = (e) => {
+    if (player.playing) {
+      player.pause()
+    } else {
+      player.play({ streamUrl: track.stream_url})
+    }
+  }
+
+  const prev = (e) =>{
+    if(index>0)
+      setIndex( i => i-1)
+  }
+
+  const next = (e) =>{
+    if(index<tracks.length-1)
+      setIndex( i => i+1)
+  }
+
+  const onProgressClick = (e) =>{
+    //trigger play if not playing already
+    if (!player.playing)
+      player.play({ streamUrl: track.stream_url})
+
+    const xPos = (e.pageX - e.currentTarget.getBoundingClientRect().left) / e.currentTarget.offsetWidth
+    const time = xPos*track.duration/1000
+    player.setTime(time)
+  }
+
+  const onVolumeChange = (e) =>{
+    const vol = parseInt(e.target.value)
+    const mut = (vol === 0)
+    player.audio.volume = (vol / 100)
+    player.audio.muted = mut
+    setVolume(vol)
+    setMuted(mut)
+  }
+
+  const onMuteClick = (e) =>{
+    const mut = (!muted)
+    player.audio.muted = mut
+    setMuted(mut)
+  }
+
+  useEffect(()=>{
+    //setPlayer(new SoundCloudAudio(clientId))
+  },[])
+
+  useEffect(()=>{
+    //do not run until player is set
+    if(player===null)
+      return
+
+    player.on('ended', next)
+    player.on('timeupdate', () => {
+      setCurrentTime(player.audio.currentTime)
+    })
+    //cleanup
+    return ()=>{
+      player.stop()
+      player.unbindAll()
+      setPlayer(null)
+    }
+  },[player])
+
 
   useEffect(()=>{
     setTrack(tracks[index].node)
+
+    //do not run until player is set
+    if(player===null)
+      return
+
+    player.play({ streamUrl: tracks[index].node.stream_url})
   },[index])
 
 
@@ -27,7 +113,7 @@ const MusicIndex = ({ data, location }) => {
       </p>
       <div className="playlist-player">
         <div className="track-details">
-          <div className="details">
+          <div className="details"  onClick={onProgressClick}>
             <div className="text">
               <h3>{track.title}</h3>
               <p>
@@ -38,27 +124,34 @@ const MusicIndex = ({ data, location }) => {
               </p>
             </div>
             <div className="progress">
-              <div className="fill" style={{width: `50%`}} />
+              <div className="fill" style={{width: `${currentTime*1000*100/track.duration}%`}} />
               <div className="waveform" style={{backgroundImage: `url(${track.waveform_url})`}} />
             </div>
           </div>
-          <div className="image" >
+          <div className="image" onClick={toggle}>
             <img src={track.artwork_url.replace('large.jpg','t300x300.jpg')} />
           </div>
         </div>
         <nav>
-          <FaVolumeOff />
+          <FaStepBackward onClick={prev}/>
+          {player && player.playing ?
+            <FaPause onClick={toggle}/>
+            :<FaPlay onClick={toggle}/>}
+          <FaStepForward onClick={next}/>
+          {muted ?
+             <FaVolumeOff onClick={onMuteClick} />
+             : <FaVolumeOn onClick={onMuteClick} />}
           <div className="volume-slider" >
-            <input type="range" min="0" max="100" step="1" value="50" readOnly />
+            <input type="range" min="0" max="100" step="1" value={muted? 0 : volume} onChange={onVolumeChange} />
           </div>
-          <span className="timer">00:00</span>
+          <span className="timer"> {prettyTime(currentTime)}</span>
         </nav>
         <ul>
         {tracks.map(({ node },i) => {
             return (
               <li key={i} className={i===index?'active':''} onClick={() => setIndex(i)}>
                   <span className="title">{node.title}</span>
-                  <span className="time">00:00</span>
+                  <span className="time">{prettyTime(node.duration / 1000)}</span>
               </li>
             )
           })}
