2eb635d4b04183fc1441c1ed87f4ebd07c09aa39
Diego Dorado <diegodorado@gmail.com>
2019-07-29 00:02:56 -0300

    adds some labs pages

---
+++ src/pages/labs/live-emojing.js
@@ -0,0 +1,29 @@
+import React, {useContext} from "react"
+import Layout from "../../layouts/live-emojing"
+import SEO from "../../components/seo"
+import Playground from '../../components/live-emojing/playground'
+import Connection from '../../components/live-emojing/connection'
+import LiveEmojingStore from '../../components/live-emojing/store'
+import LiveEmojingContext from '../../components/live-emojing/context.js'
+
+const LiveEmojingWithContext = ({pattern}) =>{
+  const {configuring} = useContext(LiveEmojingContext)
+  return configuring? <Connection /> : <Playground pattern={pattern} />
+}
+
+const LiveEmojingIndex = ({location}) =>{
+  const parts = location.hash.split('#')
+  const pattern = (parts.length >1) ? decodeURIComponent(parts[1]) : ''
+
+  return (
+    <Layout location={location} >
+      <SEO title="live emojing" />
+      <LiveEmojingStore>
+        <LiveEmojingWithContext pattern={pattern} />
+      </LiveEmojingStore>
+    </Layout>
+  )
+
+}
+
+export default LiveEmojingIndex
